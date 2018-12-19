79d402496a232c29c2829fdcd2bd12cd4604345a
Shyam <m.shyam.tnj@gmail.com>
2018-12-19 21:13:14 +0530

    Learnt Intent properly

--- MyApplication/app/src/main/java/com/example/shyam/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/shyam/myapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.shyam.myapplication;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -12,13 +11,30 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity  {
 
 
     TextView result;
     TextView multiplier_text;
     SeekBar multiplier;
     EditText input;
+
+    private SeekBar.OnSeekBarChangeListener seekbarControl = new SeekBar.OnSeekBarChangeListener() {
+        public void onProgressChanged(SeekBar s, int progress, boolean fromUser)
+        {
+            multiplier_text.setText(" : " + multiplier.getProgress());
+        }
+
+        public void onStartTrackingTouch(SeekBar s)
+        {
+
+        }
+
+        public void onStopTrackingTouch(SeekBar s)
+        {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         multiplier_text = findViewById(R.id.multiplier_text);
 
         multiplier_text.setText(" : " + multiplier.getProgress());
-        multiplier.setOnSeekBarChangeListener(this);
+        multiplier.setOnSeekBarChangeListener(seekbarControl);
     }
 
     public void parseResponse(View v)
@@ -48,43 +64,18 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         result.setText("" +  a*b);
     }
 
-    void changeValue()
-    {
-        multiplier_text.setText(" : " + multiplier.getProgress());
-    }
 
-    public void onProgressChanged(SeekBar s, int progress, boolean fromUser)
-    {
-        changeValue();
-    }
-
-    public void onStartTrackingTouch(SeekBar s)
-    {
-
-    }
-
-    public void onStopTrackingTouch(SeekBar s)
-    {
-
-    }
     public void feedback(View v)
     {
-        String[] email = {"m.shyam.tnj@gmail.com"};
-        String subject = "A feedback!";
-
-        Intent I = new Intent (Intent.ACTION_SENDTO);
-        I.setData(Uri.parse("mailto:"));
-        I.putExtra(Intent.EXTRA_EMAIL, email);
-        I.putExtra(Intent.EXTRA_SUBJECT, subject);
-        I.putExtra(Intent.EXTRA_TEXT, "Hello, i got the result " +  result.getText());
-
+        Intent I = new Intent();
+        I.setClass(this, feebackActivity.class);
         if (I.resolveActivity(getPackageManager()) != null)
         {
             startActivity(I);
         }
         else
         {
-            Toast.makeText(this, "Error no email app", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Error starting feedback activity", Toast.LENGTH_LONG).show();
         }
     }
 
--- MyApplication/app/src/main/java/com/example/shyam/myapplication/feebackActivity.java
+++ MyApplication/app/src/main/java/com/example/shyam/myapplication/feebackActivity.java
@@ -2,12 +2,25 @@ package com.example.shyam.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class feebackActivity extends AppCompatActivity {
+
+    private TextView text;
+    private LinearLayout rootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feeback);
+
+        rootView = findViewById(R.id.feedbackRootView);
+        text = new TextView(this);
+        text.setText("Hey this is the feedback form!");
+        text.setPadding(8,8,8,8);
+        La
+        rootView.addView(text);
+
     }
 }
